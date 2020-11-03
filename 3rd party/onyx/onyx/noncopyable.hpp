ffda1df7c95d29b4ba87528bb5af638610c23fd5
Schtainhardt <yaroslav_a_p@mail.ru>
2020-11-03 14:39:18 +0200

    [ADD] onyx

---
+++ 3rd party/onyx/onyx/noncopyable.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace onyx
+{
+	class noncopyable 
+	{
+	public:
+		noncopyable() = default;
+		~noncopyable() = default;
+
+	private:
+		noncopyable(const noncopyable&) = delete;
+		noncopyable& operator=(const noncopyable&) = delete;
+	};
+}
